d764c144f2771366a5fb882178d90471acb27d90
sRyb <ryb.anton.le@gmail.com>
2021-12-29 23:41:35 +0200

    proj structure

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp');
+    sass = require('gulp-sass')(require('sass')),
+    watch  = require('gulp-watch');
+   
+
+
+gulp.task('sass', async function(){
+    return gulp.src('src/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('public/css'))
+    });
+
+
+
+gulp.task('js', async function(){
+    return gulp.src('src/js/*.js')
+    .pipe(gulp.dest('public/js'))
+})
+
+gulp.task('html', async function(){
+    return gulp.src('src/*.html')
+    .pipe(gulp.dest('public/'))
+})
+
+gulp.task('imagemin', async function(){
+    return gulp.src('src/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('public/img'))
+});
+
+
+gulp.task('watch', function(){
+    gulp.watch('src/sass/**/*.scss', gulp.parallel('sass'));   
+    gulp.watch('src/js/*.js', gulp.parallel('js'));
+    gulp.watch('src/*.html', gulp.parallel('html')); 
+});
+
